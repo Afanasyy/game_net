2a1245d755eadf703346a2c3a12f44c5aa202d0c
FarguSS <66055931+Afanasyy@users.noreply.github.com>
2021-11-16 16:28:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,25 +12,75 @@ app.get("/start", (req, res) => {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (var i = 0; i < 15; i++)
     a += possible[Math.floor(Math.random() * possible.length)];
-  game[a] = { a: "", b: "" };
+  game[a] = {
+    a: 0,
+    b: 0,
+    start: -2,
+    a_x: 0,
+    a_y: 0,
+    a_d_x: 0,
+    a_d_y: 0,
+    b_x: 0,
+    b_y: 0,
+    b_d_x: 0,
+    b_d_y: 0,
+  };
 
   console.log(game.a);
 
   res.redirect("/game.html?valid=" + a);
 });
 
+app.get("/startGame", (req, res) => {
+  game[req.query.room].start += 1;
+  res.send("" + game[req.query.room].start);
+});
+
+app.get("/get_pos_ball", (req, res) => {
+  console.log(req.query);
+  if (req.query.uid == 0) {
+    game[req.query.room].a_x = req.query.x;
+    game[req.query.room].a_y = req.query.y;
+    game[req.query.room].a_d_x = req.query.dx;
+    game[req.query.room].a_d_y = req.query.dy;
+    res.send({
+      id: 1,
+      x: game[req.query.room].b_x,
+      y: game[req.query.room].b_y,
+      dx: game[req.query.room].b_d_x,
+      dy: game[req.query.room].b_d_y,
+    });
+  } else if (req.query.uid == 1) {
+    game[req.query.room].b_x = req.query.x;
+    game[req.query.room].b_y = req.query.y;
+    res.send({
+      id: 0,
+      x: game[req.query.room].a_x,
+      y: game[req.query.room].a_y,
+      dx: game[req.query.room].a_d_x,
+      dy: game[req.query.room].a_d_y,
+    });
+  }
+});
+
 app.get("/game_update", (req, res) => {
+  if (game[req.query.room].start < 0) {
+    res.send("" + game[req.query.room].start);
+    return;
+  }
   //console.log(req.query);
 
   if (req.query.uid == 0) {
-    game[req.query.room].a += req.query.bt;
+    //if (req.query.bt != "0")
+    game[req.query.room].a = req.query.bt;
+
     res.send(game[req.query.room].b);
-    game[req.query.room].b = "";
     console.log(game);
   } else if (req.query.uid == 1) {
-    game[req.query.room].b += req.query.bt;
+    //if (req.query.bt != "0")
+    game[req.query.room].b = req.query.bt;
+
     res.send(game[req.query.room].a);
-    game[req.query.room].a = "";
     console.log(game);
   }
 });
--- public/index2.js
+++ public/index2.js
@@ -2,6 +2,9 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var gameRoom = window.location.href.split("=")[1];
 
+var name_t = prompt();
+var keyBuf = -2;
+var ballp = {};
 var players = [
   {
     id: 0,
@@ -28,7 +31,6 @@ var players = [
     ballDel: { x: 2, y: -2 },
   },
 ];
-
 var ballRadius = 10;
 
 var paddleHeight = 10;
@@ -124,6 +126,7 @@ function collisionDetection() {
 }
 
 function draw() {
+  if (keyBuf < 0 || keyBuf == "") return;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
   drawBall();
@@ -133,18 +136,34 @@ function draw() {
     if (
       players[i].ballPos.x + players[i].ballDel.x > canvas.width - ballRadius ||
       players[i].ballPos.x + players[i].ballDel.x < ballRadius
-    )
+    ) {
       players[i].ballDel.x *= -1;
+      if (i == name_t) foo(i);
+      else {
+        players[i].ballDel.x = ballp.dx;
+        players[i].ballDel.y = ballp.dy;
+      }
+    }
     if (
       players[i].ballPos.y + players[i].ballDel.y >
         canvas.height - ballRadius - paddleHeight &&
       players[i].ballPos.x + players[i].ballDel.x <
         players[i].posX + paddleWidth &&
       players[i].ballPos.x + players[i].ballDel.x > players[i].posX
-    )
+    ) {
       players[i].ballDel.y *= -1;
-    else if (players[i].ballPos.y + players[i].ballDel.y < ballRadius) {
+      if (i == name_t) foo(i);
+      else {
+        players[i].ballDel.x = ballp.dx;
+        players[i].ballDel.y = ballp.dy;
+      }
+    } else if (players[i].ballPos.y + players[i].ballDel.y < ballRadius) {
       players[i].ballDel.y *= -1;
+      if (i == name_t) foo(i);
+      else {
+        players[i].ballDel.x = ballp.dx;
+        players[i].ballDel.y = ballp.dy;
+      }
     } else if (
       players[i].ballPos.y + players[i].ballDel.y >
       canvas.height - ballRadius
@@ -153,7 +172,13 @@ function draw() {
             document.location.reload();
             clearInterval(interval); // Needed for Chrome to end game*/
       players[i].ballDel.y *= -1;
+      if (i == name_t) foo(i);
+      else {
+        players[i].ballDel.x = ballp.dx;
+        players[i].ballDel.y = ballp.dy;
+      }
     }
+
     if (
       players[i].rightPressed &&
       players[i].posX < canvas.width - paddleWidth
@@ -161,6 +186,28 @@ function draw() {
       players[i].posX += 7;
     } else if (players[i].leftPressed && players[i].posX > 0) {
       players[i].posX -= 7;
+    }
+
+    if (
+      (players[i].rightPressed &&
+        players[i].posX < canvas.width - paddleWidth) ||
+      (players[i].leftPressed && players[i].posX > 0 && i == name_t)
+    ) {
+      httpGetAsync(
+        "/game_update?room=" +
+          gameRoom +
+          "&uid=" +
+          players[i].id +
+          "&bt=" +
+          players[i].posX,
+        (test) => {
+          console.log(test);
+        }
+      );
+    }
+
+    if (i != name_t) {
+      players[i].posX = keyBuf;
     }
 
     players[i].ballPos.x += players[i].ballDel.x;
@@ -172,9 +219,25 @@ function draw() {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onload = function () {};
 }
-
-var interval = setInterval(draw, 50);
-
+function foo(id) {
+  httpGetAsync(
+    "/get_pos_ball?room=" +
+      gameRoom +
+      "&uid=" +
+      players[id].id +
+      "&x=" +
+      players[id].ballPos.x +
+      "&y=" +
+      players[id].ballPos.y +
+      "&dx=" +
+      players[id].ballDel.x +
+      "&dy=" +
+      players[id].ballDel.y,
+    (test) => {
+      ballp = JSON.parse(test);
+    }
+  );
+}
 function httpGetAsync(theUrl, callback) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function () {
@@ -190,21 +253,63 @@ document.addEventListener("keydown", keyDownHandler, false);
 function keyDownHandler(e) {
   for (var i = 0; i < players.length; i++) {
     if (e.keyCode == players[i].keyRight) {
-      httpGetAsync(
-        "/game_update?room=" + gameRoom + "&uid=" + players[i].id + "&bt=R",
-        (test) => {
-          console.log(test);
-        }
-      );
-      players[i].rightPressed = true;
+      if (players[i].rightPressed == false && i == name_t)
+        players[i].rightPressed = true;
     } else if (e.keyCode == players[i].keyLeft) {
-      httpGetAsync(
-        "/game_update?room=" + gameRoom + "&uid=" + players[i].id + "&bt=L",
-        (test) => {
-          console.log(test);
-        }
-      );
-      players[i].leftPressed = true;
+      if (players[i].rightPressed == false && i == name_t)
+        players[i].leftPressed = true;
     }
   }
 }
+
+httpGetAsync("/startGame?room=" + gameRoom, (test) => {
+  keyBuf = test;
+
+  var tring = true;
+
+  setInterval(() => {
+    httpGetAsync(
+      "/game_update?room=" +
+        gameRoom +
+        "&uid=" +
+        name_t +
+        "&bt=" +
+        players[name_t].posX,
+      (test) => {
+        keyBuf = test;
+
+        if (test >= 0 && tring == true) {
+          tring = false;
+          players = [
+            {
+              id: 0,
+              color: "red",
+              keyLeft: "37",
+              keyRight: "39",
+              posX: 130,
+              score: 0,
+              rightPressed: false,
+              leftPressed: false,
+              ballPos: { x: canvas.width / 2 - 25, y: canvas.height - 30 },
+              ballDel: { x: -2, y: -2 },
+            },
+            {
+              id: 1,
+              color: "blue",
+              keyLeft: "65",
+              keyRight: "68",
+              posX: 300,
+              score: 0,
+              rightPressed: false,
+              leftPressed: false,
+              ballPos: { x: canvas.width / 2 + 25, y: canvas.height - 30 },
+              ballDel: { x: 2, y: -2 },
+            },
+          ];
+        }
+      }
+    );
+  }, 100);
+
+  setInterval(draw, 50);
+});
